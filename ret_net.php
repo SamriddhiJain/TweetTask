4c8d69f0bfb5d4c3e2a9043026950382b035e000
Samriddhi Jain <j.samriddhi13@gmail.com>
2016-02-02 15:19:56 +0530

    keys updated

--- ret_net.php
+++ ret_net.php
@@ -4,10 +4,10 @@
 	require_once("twitteroauth-master/twitteroauth/twitteroauth.php");
 	/** Set access tokens here - see: https://dev.twitter.com/apps/ **/
 	
-	$consumerkey = "s32ku8Q9COXIZc84MoE71j9wK"; 
-	$consumersecret = "6yKHtC4onmBx5vBenEj4uQwCXwL9omwKeX2dRqBy9PC6WeDXG5"; 
-	$accesstoken = "4733276482-5JSpMp8KqjhhiELjez8FpIdy2ZzClnUVaL7ru54"; 
-	$accesstokensecret = "BcUvJ6U4frtu5ITbgOzYmNY4dU0RDiSinRNXUK7DV8nlX";
+	$consumerkey = ""; 
+	$consumersecret = ""; 
+	$accesstoken = ""; 
+	$accesstokensecret = "";
 	$notweets = 20;
 
 	function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
